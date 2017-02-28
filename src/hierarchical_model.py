b996281adc9aa5059dc777f4c3738215fdf0a7de
brendan-drew <brendandrew@Brendans-MacBook-Air.local>
2017-02-28 16:22:15 -0700

    Updated the target specification in the hierarchical model and added notes/comments

--- src/hierarchical_model.py
+++ src/hierarchical_model.py
@@ -4,9 +4,12 @@ import pymc3 as pm
 import matplotlib.pyplot as plt
 import cPickle as pickle
 from sklearn.preprocessing import StandardScaler, Imputer
+import pdb
 
 class HierarchicalModel(object):
-
+    '''
+    This class takes a pandas dataframe with a specified target and scaled/unscaled features of interest, builds a Pymc3 hierarchical model of linear models forecasting the target at the individual county level, and constructs dataframes with the model parameters and annual predictions for each county.
+    '''
     def __init__(self, data, target, unscaled_features, scaled_features, train_cutoff = 6, progressbar = True, subset = 500):
         '''
         INPUT:
@@ -42,9 +45,9 @@ class HierarchicalModel(object):
         else:
             self.n_counties = len(self.counties)
         # Array of target values for training
-        self.target_train = self.data.loc[self.data['year'] < self.train_cutoff, target].values
+        self.target_train = self.data.loc[self.data['year'] < self.train_cutoff, self.target].values
         # Array of target values for testing
-        self.target_test = self.data.loc[self.data['year'] >= self.train_cutoff, target].values
+        self.target_test = self.data.loc[self.data['year'] >= self.train_cutoff, self.target].values
         # StandardScaler object used to transform features
         self.features_matrix, self.scaler = self.build_features_matrix()
         # Instantiate a dataframe to hold county-level fips and coefficients
@@ -55,30 +58,49 @@ class HierarchicalModel(object):
         self.hyperprior_names, self.prior_names = self.get_prior_names()
         # Get the model trace
         self.trace, self.model = self.build_model()
-        # Instantiate a dataframe to hold coefficients, forecast, and error calculations for all tested counties and all years
+        # Instanciate a dataframe to hold coefficients, forecast, and error calculations for all tested counties and all years
         self.evaluation_df = self.build_evaluation_df()
         # Get the scores for model evaluation
         self.rmse_2014, self.rmse_2013, self.residual_rmse, self.dic, self.waic = self.score_model()
 
 
     def build_data_frame(self, data):
+        '''
+        INPUT: Pandas dataframe passed during class instanciation
+        OUTPUT: Class attribute pandas dataframe with the target and features of interest - NA VALUES ARE DROPPED
+        '''
+        # Subset the randomly selected counties
         data['subset_flag'] = data['state_and_county_fips_code'].apply(lambda x: 1 if x in self.counties else 0)
-        return data.loc[data['subset_flag'] == 1, ['state_and_county_fips_code', 'year'] + self.coef_list + [self.target]].dropna()
+        # Return the subset with the variables of interest
+        return data.loc[data['subset_flag'] == 1, ['state_and_county_fips_code'] + self.coef_list + [self.target]].dropna()
 
     def build_features_matrix(self):
+        '''
+        INPUT: None
+        OUTPUT: Builds a features matrix (np array) from pandas dataframe including target and features of interest
+        '''
+        # Select unscaled features
         if self.unscaled_features:
             unscaled_train_features = self.data.loc[self.data['year'] < self.train_cutoff, self.unscaled_features].values
+        # Select and transform scaled features
         if self.scaled_features:
             scaler = StandardScaler()
             scaled_train_features = scaler.fit_transform(self.data.loc[self.data['year'] < self.train_cutoff, self.scaled_features].values)
+        # Merge scaled and unscaled features in a matrix
         if self.unscaled_features and self.scaled_features:
             return np.hstack((unscaled_train_features, scaled_train_features)), scaler
+        # If no unscaled features, return scaled features matrix
         elif self.scaled_features:
             return scaled_train_features, scaler
+        # If no scaled features, return unscaled features matrix
         else:
-            return unscaled_train_features, scaler
+            return unscaled_train_features, None
 
     def get_prior_names(self):
+        '''
+        INPUT: None
+        OUTPUT: Creates a list of coefficient names to be passed to Pymc3 hierarchical model for evaluation
+        '''
         # Get a list of hyperprior names
         hyperprior_names = zip(['mu_beta_' + x for x in self.coef_list], ['sigma_beta_' + x for x in self.coef_list])
         # Get a list of coefficient names
@@ -86,12 +108,20 @@ class HierarchicalModel(object):
         return hyperprior_names, prior_names
 
     def get_train_labels(self):
+        '''
+        INPUT: None
+        OUTPUT: List of 0-indexed labels corresponding to the unique counties in self.data, required for building the hierarchical model
+        '''
         # Get the counties from the training set
         train_counties = self.data.loc[self.data['year'] < 6, 'state_and_county_fips_code']
         # Convert the fips values to their 0-indexed corresponding value in the lookup df
         return [self.lookup_df.loc[x][0] for x in train_counties.values]
 
     def build_model(self):
+        '''
+        INPUT: None
+        OUTPUT: Builds a hierarchical model using the target and variables of interest, and returns the trace with the model
+        '''
         # This approach is taken from http://twiecki.github.io/blog/2014/03/17/bayesian-glms-3/
         with pm.Model() as hierarchical_model:
             # Hyperprior for the intercept mean
@@ -125,6 +155,10 @@ class HierarchicalModel(object):
             return pm.sample(5000, step, start = mu), hierarchical_model
 
     def build_evaluation_df(self):
+        '''
+        INPUT: None
+        OUTPUT: Returns a pandas dataframe with the mean, upper bound, and lower bound of all linear model parameters, as well as the target prediction for each county-year
+        '''
         # Get all values for the intercept
         alphas = self.trace.get_values('alpha', burn = 500)
         # Instantiate an empty list to store the values for beta
@@ -132,7 +166,7 @@ class HierarchicalModel(object):
         # Append data for each coefficient in a nested list
         for i, coef in enumerate(self.coef_list):
             betas.append(self.trace.get_values('beta_' + coef, burn = 500))
-        # Instantiate empty lists to store the mean, upper bound, and lower bound (95% CI) for the intercept and all coefficients
+        # Instanciate empty lists to store the mean, upper bound, and lower bound (95% CI) for the intercept and all coefficients
         alpha_means = []
         alpha_upper_bounds = []
         alpha_lower_bounds = []
@@ -174,10 +208,14 @@ class HierarchicalModel(object):
             predictions += output_df['beta_' + var + '_mean'] * transformed_features[:,i]
         # Append the predictions to the dataframe and calculate residuals
         output_df['predictions'] = predictions
-        output_df['residuals'] = output_df['predictions'] - output_df['actual_per_capita_costs']
+        output_df['residuals'] = output_df['predictions'] - output_df[self.target]
         return output_df
 
     def score_model(self):
+        '''
+        INPUT: None
+        OUTPUT: Calculates the 2013 and 2014 forecast error of the model, as well as the model DIC and WAIC scores
+        '''
         # Calculate the residual rmse and 2013 & 2014 test RMSE
         rmse_2014 = np.sqrt(np.mean(self.evaluation_df.loc[self.evaluation_df['year'] == 7, 'residuals']**2))
         rmse_2013 = np.sqrt(np.mean(self.evaluation_df.loc[self.evaluation_df['year'] == 6, 'residuals']**2))
@@ -189,10 +227,16 @@ class HierarchicalModel(object):
         return rmse_2014, rmse_2013, residual_rmse, dic, waic
 
     def get_results(self):
+        '''
+        INPUT: None
+        OUTPUT: Returns a dictionary of the coefficients, residual error, 2013 & 2014 forecast error and DIC and WAIC scores for the model. To be used in building a table of model results.
+        '''
         return {'coefficients': self.coef_list, 'residual_error': self.residual_rmse, 'rmse_2013': self.rmse_2013, 'rmse_2014': self.rmse_2014, 'dic': self.dic, 'waic': self.waic}
 
 class NationalModel(object):
-
+    '''
+    Constructs unique hierarchical linear models predicting a target from one or more features of interest for each state.
+    '''
     def __init__(self, data, target, county_subset_size = None, unscaled_features = [], scaled_features = [], train_cutoff = 6, progressbar = True):
         self.states = data['state'].unique()
         self.data = data
@@ -226,10 +270,4 @@ class NationalModel(object):
 
 
 if __name__ == '__main__':
-    data = pd.read_csv('data/medicare_county_level/cleaned_medicare_county_all.csv')
-    data['year'] = data['year'] - 2007
-    data['years_post_aca'] = data['year'].apply(lambda x: x - 3 if x > 3 else 0)
-    unscaled_features = ['years_post_aca']
-    scaled_features = ['ip_covered_stays_per_1000_beneficiaries']
-    np.random.seed(123)
-    hm = HierarchicalModel(data, 'actual_per_capita_costs', unscaled_features, scaled_features, subset = 500)
+    pass
